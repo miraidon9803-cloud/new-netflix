2c258112f397a7c96eb5f134f1cc8174badaae41
alsrl980409 <miraidon9803@gmail.com>
2025-12-05 18:02:23 +0900

    넷플릭스 오리지날

--- src/components/NetflixOriginal.tsx
+++ src/components/NetflixOriginal.tsx
@@ -1,4 +1,11 @@
-const NetflixOriginal = ({ title, original }) => {
+import type { Original } from "../types/netflix";
+import "./scss/NetflixOriginal.scss";
+
+interface NetflixOriginalProps {
+  title: string;
+  original: Original[];
+}
+const NetflixOriginal = ({ title, original }: NetflixOriginalProps) => {
   return (
     <div>
       <div className="inner">
@@ -6,7 +13,7 @@ const NetflixOriginal = ({ title, original }) => {
           <h2>{title}</h2>
           <ul className="list">
             {original.map((data) => (
-              <li key={data.id}>
+              <li className="origianl-list" key={data.id}>
                 <div>
                   <img
                     src={`https://image.tmdb.org/t/p/w500${data.backdrop_path}`}
