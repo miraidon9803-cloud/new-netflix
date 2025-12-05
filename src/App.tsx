56dbba4dce273c28209c1dd18fe676f2d9a5735f
alsrl980409 <miraidon9803@gmail.com>
2025-12-05 15:19:15 +0900

    기본세팅하기

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Routes } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Routes></Routes>
+    </>
+  );
+}
+
+export default App;
