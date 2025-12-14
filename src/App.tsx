acfd389e1bd715d52e68c9b361d9f9218a827b31
jeon woo jun <a01039777440@gmail.com>
2025-12-15 01:13:20 +0900

    12월15일 shots작업

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header";
 import Main from "./pages/Main";
@@ -6,19 +6,25 @@ import Login from "./pages/Login";
 import Join from "./pages/Join";
 import Footer from "./components/Footer";
 import Membership from "./pages/Membership";
+import Shorts from "./components/Shorts";
 
 function App() {
+  const location = useLocation();
+  const isShorts = location.pathname === '/shorts';
+
   return (
     <>
+      {/* Shorts 페이지가 아닐 때만 Header 표시, 또는 430px 이하에서 Shorts일 때 숨김 처리 */}
       <Header />
       <Routes>
         <Route path="/" element={<Main />} />
-
+        <Route path="/shorts" element={<Shorts />} />
         <Route path="login" element={<Login />} />
         <Route path="join" element={<Join />} />
         <Route path="membership" element={<Membership />} />
       </Routes>
-      <Footer />
+      {/* Shorts 페이지에서는 Footer 숨김 */}
+      {!isShorts && <Footer />}
     </>
   );
 }
