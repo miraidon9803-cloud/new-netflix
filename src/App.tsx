d62121ff17ead5f9543759102237f202f04cf66a
alsrl980409 <miraidon9803@gmail.com>
2025-12-15 14:12:24 +0900

    안쓰는거지움

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,17 @@
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
 
-import Main from './pages/Main';
-import Login from './pages/Login';
-import Join from './pages/Join';
-import Membership from './pages/Membership';
-import Mypage from './pages/Mypage';
-import Layout from './pages/Layout';
+import Main from "./pages/Main";
+
+import Mypage from "./pages/Mypage";
+import Layout from "./pages/Layout";
+import FullLogin from "./pages/FullLogin";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Main />} />
-        <Route path="membership" element={<Membership />} />
         <Route path="mypage" element={<Mypage />} />
         <Route path="auth" element={<FullLogin />} />
       </Route>
--- src/store/NetflixStore.ts
+++ src/store/NetflixStore.ts
@@ -1,8 +1,8 @@
-import { create } from 'zustand';
-import type { NetflixState, MediaItem } from '../types/netflix';
+import { create } from "zustand";
+import type { NetflixState, MediaItem } from "../types/netflix";
 
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
-const BASE_URL = 'https://api.themoviedb.org/3';
+const BASE_URL = "https://api.themoviedb.org/3";
 
 export const useNetflixStore = create<NetflixState>((set) => ({
   original: [],
@@ -19,11 +19,11 @@ export const useNetflixStore = create<NetflixState>((set) => ({
       );
       const data = await res.json();
 
-      console.log('🟥 넷플릭스 오리지널:', data.results);
+      console.log("🟥 넷플릭스 오리지널:", data.results);
 
       set({ original: data.results });
     } catch (err) {
-      console.error('넷플릭스 오리지널 오류:', err);
+      console.error("넷플릭스 오리지널 오류:", err);
     }
   },
 
@@ -38,50 +38,54 @@ export const useNetflixStore = create<NetflixState>((set) => ({
       const top10: MediaItem[] = (data.results || []).slice(0, 10);
 
       // 콘솔로 서버 데이터 확인
-      console.log('🟦 넷플릭스 인기 TOP10 TV (원본):', data);
-      console.log('🟩 TOP10 배열:', top10);
+      console.log("🟦 넷플릭스 인기 TOP10 TV (원본):", data);
+      console.log("🟩 TOP10 배열:", top10);
       console.log(
-        '🟨 TOP10 이름 목록:',
+        "🟨 TOP10 이름 목록:",
         top10.map((t) => t.name)
       );
 
       set({ netflixTop10: top10 });
     } catch (err) {
-      console.error('넷플릭스 TOP10 오류:', err);
+      console.error("넷플릭스 TOP10 오류:", err);
     }
   },
 
   //  3. 오늘의 트렌딩 영화 TOP10 가져오기
   onFetchMovieTop10: async () => {
     try {
-      const res = await fetch(`${BASE_URL}/trending/movie/day?api_key=${API_KEY}&language=ko-KR`);
+      const res = await fetch(
+        `${BASE_URL}/trending/movie/day?api_key=${API_KEY}&language=ko-KR`
+      );
 
       const data = await res.json();
 
       const top10: MediaItem[] = (data.results || []).slice(0, 10);
 
-      console.log('🎬 오늘의 트렌딩 영화 TOP10:', top10);
+      console.log("🎬 오늘의 트렌딩 영화 TOP10:", top10);
 
       set({ movieTop10: top10 });
     } catch (err) {
-      console.error('영화 TOP10 오류:', err);
+      console.error("영화 TOP10 오류:", err);
     }
   },
   //  4. 오늘의 트렌딩 시리즈 TOP10 가져오기
   onFetchSeriesTop10: async () => {
     try {
-      const res = await fetch(`${BASE_URL}/trending/tv/day?api_key=${API_KEY}&language=ko-KR`);
+      const res = await fetch(
+        `${BASE_URL}/trending/tv/day?api_key=${API_KEY}&language=ko-KR`
+      );
       // https://api.themoviedb.org/3/trending/tv/day?language=en-US
 
       const data = await res.json();
 
       const stop10: MediaItem[] = (data.results || []).slice(0, 10);
 
-      console.log('🎬 오늘의 트렌딩 시리즈 TOP10:', stop10);
+      console.log("🎬 오늘의 트렌딩 시리즈 TOP10:", stop10);
 
       set({ SeriesTop10: stop10 });
     } catch (err) {
-      console.error('영화 TOP10 오류:', err);
+      console.error("영화 TOP10 오류:", err);
     }
   },
   onFetchSFTop10: async () => {
@@ -95,11 +99,11 @@ export const useNetflixStore = create<NetflixState>((set) => ({
 
       const sftop10: MediaItem[] = (data.results || []).slice(0, 10);
 
-      console.log('🎬 오늘의 SF&Fantasy 시리즈 TOP10:', sftop10);
+      console.log("🎬 오늘의 SF&Fantasy 시리즈 TOP10:", sftop10);
 
       set({ SFNFTop10: sftop10 });
     } catch (err) {
-      console.error('SF TOP10 오류:', err);
+      console.error("SF TOP10 오류:", err);
     }
   },
 }));
