1279e0699dffa1a4aeb320b6a1bec7be6284f5e3
LYB <leeyb970228@gmail.com>
2025-12-22 13:32:00 +0900

    fix:usememo 비활성화

--- src/components/Movies/FilterPopup.tsx
+++ src/components/Movies/FilterPopup.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from "react";
+import React, {  useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useMovieFilterStore } from "../../store/useMovieFilterStore";
 import "./scss/FilterPopup.scss";
