2bfcdc367884b05a43e7855bbf07c032c46e5a6f
alsrl980409 <miraidon9803@gmail.com>
2025-12-22 21:54:23 +0900

    롤백2

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,21 +1,22 @@
-import { useEffect } from 'react';
-import './scss/Main.scss';
+import { useEffect } from "react";
+import "./scss/Main.scss";
 // import NetflixOriginal from '../components/NetflixOriginal';
-import { useNetflixStore } from '../store/NetflixStore';
-import MainBanner from '../components/MainBanner';
-import InfiniSubmenu from '../components/InfiniSubmenu';
-import Originals from '../components/Originals';
-import NowPlay from '../components/NowPlay';
-import ComingSoon from '../components/ComingSoon';
-import WalkingDead from '../components/WalkingDead';
-import SideNav from '../components/SideNav';
-import StrangerNew from '../components/StrangerNew';
-import TodayTop10 from '../components/TodayTop10';
-import MovieTop10 from '../components/MovieTop10';
-import SFNFantasy from '../components/SFNFantasy';
-import TrandingSeries from '../components/TrandingSeries';
-import ChoiceContentTxt from '../components/ChoiceContentTxt';
-import MobileNav from '../components/MobileNav';
+import { useNetflixStore } from "../store/NetflixStore";
+import MainBanner from "../components/MainBanner";
+import InfiniSubmenu from "../components/InfiniSubmenu";
+import Originals from "../components/Originals";
+import NowPlay from "../components/NowPlay";
+import ComingSoon from "../components/ComingSoon";
+import WalkingDead from "../components/WalkingDead";
+import SideNav from "../components/SideNav";
+import StrangerNew from "../components/StrangerNew";
+import TodayTop10 from "../components/TodayTop10";
+import MovieTop10 from "../components/MovieTop10";
+import SFNFantasy from "../components/SFNFantasy";
+import TrandingSeries from "../components/TrandingSeries";
+import ChoiceContentTxt from "../components/ChoiceContentTxt";
+import MobileNav from "../components/MobileNav";
+import ContinueWatchingRow from "../components/ContinueWatchingRow.tsx";
 
 const Main = () => {
   // const navigate = useNavigate();
@@ -37,6 +38,7 @@ const Main = () => {
       {/* <NetflixOriginal title="넷플릭스 오리지날" original={original} /> */}
       <InfiniSubmenu />
       <MainBanner />
+      <ContinueWatchingRow />
       <TodayTop10 />
       <Originals />
       <NowPlay />
