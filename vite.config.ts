6d26edaf0c904ac10ecbcb29dea1d43b4c30254c
alsrl980409 <miraidon9803@gmail.com>
2025-12-24 12:36:06 +0900

    모바일베포

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,39 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+//vite.config.ts
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Netfilx App",
+        short_name: "Netfilx",
+        start_url: "/",
+        display: "standalone",
+        // 화면이 열릴때 보이는 배경
+        background_color: "#ffffff",
+        theme_color: "#000000",
+        icons: [
+          {
+            // src: "/Netfilx_192x192.svg",
+            // sizes: "192x192",
+            // type: "image/svg+xml",
+            src: "/netfilx-icon.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+          {
+            src: "/netfilx-icon.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable", //
+          },
+        ],
       },
     }),
   ],
-})
+});
